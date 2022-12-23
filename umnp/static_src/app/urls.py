9898328ea052d5efbdf9061b355e6312976b92f1
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-12-23 14:58:07 +0700

    added datas from previous repository

--- umnp/static_src/app/urls.py
+++ umnp/static_src/app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('works', views.portofolio, name='portofolio'),
+    path('about', views.about, name='about'),
+    path('works/<projectname>', views.portofolio_details, name='portofolio_detail'),
+    path('career', views.career, name='career'),
 ]
--- umnp/static_src/app/views.py
+++ umnp/static_src/app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'django_tailwind.html')
+    return render(request, 'index.html')
+
+def portofolio(request):
+    return render(request, 'portofolio.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def portofolio_details(request,projectname):
+    return render(
+        request,
+        'portofolio_detail.html',
+        {'projectname': projectname}
+    )
+
+def career(request):
+    return render(request, 'career.html')
