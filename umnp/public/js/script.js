fcef644cf28b8c094ce32c3dede8ec17d976d5cc
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-12-26 21:50:11 +0700

    navbar routes and responsive layout

--- umnp/public/js/script.js
+++ umnp/public/js/script.js
@@ -1,4 +1,15 @@
-function Menu(e){
-    let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+var dropdown = document.querySelector("#dropdown")
+
+function open_dropdown() {
+    dropdown.classList.remove('hidden')
+    document.body.style.height = '100vh'
+    document.body.style.overflowY = 'hidden'
 }
+
+function close_dropdown() {
+    dropdown.classList.add('hidden')
+    document.body.style.height = 'unset'
+    document.body.style.overflowY = 'unset'
+}
+
+dropdown.addEventListener('click', close_dropdown)
