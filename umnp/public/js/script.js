a5e37adfa39f91f2a1bc3b45e2d3afe7d130be96
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-12-31 19:48:04 +0700

    Contact page form redirect to mail

--- umnp/public/js/script.js
+++ umnp/public/js/script.js
@@ -1,4 +1,5 @@
-var dropdown = document.querySelector("#dropdown")
+// Navbar Dropdown
+var dropdown = document.querySelector('#dropdown')
 
 function open_dropdown() {
     dropdown.classList.remove('hidden')
@@ -13,3 +14,35 @@ function close_dropdown() {
 }
 
 dropdown.addEventListener('click', close_dropdown)
+// End of Navbar Dropdown
+
+// Contact Form Mailto
+var destinationEmails = {
+    'business': 'inquiry@umnpictures.com',
+    'career': 'career@umnpictures.com',
+    'intern': 'intern@umnpictures.com',
+}
+
+var contactForm = document.querySelector('#contact_form')
+
+function redirectMail(e) {
+    e.preventDefault()
+    var name = document.querySelector('#name').value
+    var inquiry = document.querySelector('#inquiry').value
+    var email = document.querySelector('#email').value
+    var phone = document.querySelector('#phone').value
+    var message = document.querySelector('#message').value
+
+    var subject = `${name} (${email} // ${phone})`
+    var destinationEmail = destinationEmails[inquiry]
+
+    var mailto = document.createElement('a')
+    var hrefString = `mailto:${destinationEmail}?`
+    hrefString += `subject=${subject}&`
+    hrefString += `body=${message}`
+    mailto.href = hrefString
+    mailto.click()
+}
+
+contactForm?.addEventListener('submit', redirectMail)
+// End of Contact Form Mailto
