643631654f9f7112d29dbf4a6e09e52a69c37a0c
Darren-34810 <darren.denisson@student.umn.ac.id>
2023-01-05 21:15:58 +0700

    Fixed urls

--- umnp/static_src/app/urls.py
+++ umnp/static_src/app/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('works', views.portofolio, name='portofolio'),
-    path('works/<projectname>', views.portofolio_details, name='portofolio_detail'),
-    path('career', views.career, name='career'),
-    path('contact', views.contact, name='contact'),
+    path('works/', views.portofolio, name='portofolio'),
+    path('works/<projectname>/', views.portofolio_details, name='portofolio_detail'),
+    path('career/', views.career, name='career'),
+    path('contact/', views.contact, name='contact'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
